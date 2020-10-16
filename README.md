d8205fe1de1ad76e15a452f4c3b7b1fb1625a2db
Thomas Radloff <bostontrader@gmail.com>
2020-10-16 09:22:19 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to bookwerx-common-go
+
+The purpose of bookwerx-common-go is to provide a place to factor out certain bookwerx related bits o' goodness, such as API access functions, that are of interest to more than one project.
---
+++ api.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"fmt"
+	"github.com/gojektech/heimdall/httpclient"
+	"github.com/pkg/errors"
+	"io/ioutil"
+	"net/http"
+)
+
+// Generic get on the API.  Actually this would work on any url.
+func get(client *httpclient.Client, url string) ([]byte, error) {
+
+	methodName := "bookwerx-common-go:api.go:get"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("%s :Error with http.NewRequest", methodName))
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("%s :Error with client.Do", methodName))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("%s :Error with ioutil.ReadAll", methodName))
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("%s :Error with resp.Body.Close", methodName))
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.Wrap(err, fmt.Sprintf(
+			"%s :Status code error: expected= 200, received=%, body=%s", methodName, resp.StatusCode, string(body)))
+	}
+
+	return body, nil
+
+}
+
+
